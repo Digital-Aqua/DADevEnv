72071afdea893a5da1ca32ccf23e71034bbb6235
Aaron Waters <aaron@aaronwaters.net>
2026-03-16 15:36:17 +1100

    added resolve_path to io.py

--- Common/Packages/io.py
+++ Common/Packages/io.py
@@ -1,11 +1,37 @@
 from contextlib import contextmanager
 from io import TextIOWrapper
+from pathlib import Path
 from typing import BinaryIO, Iterator, TextIO
 
 
 __all__ = [
+    'resolve_path',
     'text_writer',
 ]
+
+
+def resolve_path(
+    path: Path|str,
+    *locations: Path|str
+) -> Path:
+    """
+        Resolves a path relative to a list of locations.
+        If the path is absolute, returns it as-is.
+        If the path is relative, searches the locations
+        for the path, and returns the first match.
+        If the path is not found, raises a FileNotFoundError.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    if path.is_absolute():
+        return path
+    for search_path in locations:
+        if isinstance(search_path, str):
+            search_path = Path(search_path)
+        candidate = search_path / path
+        if candidate.is_file():
+            return candidate
+    raise FileNotFoundError(f"Path not found: {path}")
 
 
 @contextmanager
@@ -31,3 +57,4 @@ def text_writer(
     finally:
         text_writer.flush()
         text_writer.detach()
+
